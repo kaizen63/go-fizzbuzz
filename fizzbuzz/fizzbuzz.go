7b7ed50cf55b0b0258c135c9467391b8fdc69b12
Kai Poitschke <kai@poitschke.de>
2017-09-09 19:52:42 +0200

    directory restructuring

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,23 @@
+package fizzbuzz
+
+import (
+	"fmt"
+)
+
+// Fizzbuzz returns "Fizz" for every number divisibal by 3,
+// "Buzz" for every number divisable by 5,
+// "FizzBuzz" if it is divisable by 3 and 5.
+// Otherwise it returns the number
+func Fizzbuzz(i int) string {
+	var result string
+	if i%15 == 0 {
+		result = "FizzBuzz"
+	} else if i%5 == 0 {
+		result = "Buzz"
+	} else if i%3 == 0 {
+		result = "Fizz"
+	} else {
+		result = fmt.Sprint(i)
+	}
+	return result
+}
