54215206f8f4b6da83dde2a52020ab34ee6d0379
Kai Poitschke <kai@poitschke.de>
2017-09-09 22:34:57 +0200

    Testing main as process

--- main_test.go
+++ main_test.go
@@ -1,7 +1,20 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"os/exec"
+	"testing"
+)
 
 func TestMain(t *testing.T) {
-	main()
+	if os.Getenv("IN_TESTMAIN") == "1" {
+		main()
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestMain")
+	cmd.Env = append(os.Environ(), "IN_TESTMAIN=1")
+	err := cmd.Run()
+	if err != nil {
+		t.Fatalf("process ran with err %v, want exit status 0", err)
+	}
 }
